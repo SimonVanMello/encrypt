3dffb890fe96508baa8872dfada46f936295ff46
SimonVanMello <simon.vanmello@proton.me>
2024-01-26 12:26:45 +0100

    latin1 to utf8 encoding

--- src/crypto.py
+++ src/crypto.py
@@ -29,7 +29,7 @@ class Crypto:
 		"""
 		key = key.encode()
 		if decode:
-			source = base64.b64decode(source.encode("latin-1"))
+			source = base64.b64decode(source.encode("UTF-8"))
 		key = SHA256.new(key).digest()  # use SHA-256 over our key to get a proper-sized AES key
 		IV = source[:AES.block_size]  # extract the IV from the beginning
 		decryptor = AES.new(key, AES.MODE_CBC, IV)
